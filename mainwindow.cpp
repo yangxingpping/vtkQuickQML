cbfdc55f02779b290eab9fb9287ad29578ce6a9c
ggyy <alqaz@outlook.com>
2025-08-20 23:25:14 +0800

    update imp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,40 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QVTKOpenGLNativeWidget.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     ui->quickWidget->setSource(QUrl("qrc:/demo.qml"));
+    auto vtkWidget = new QVTKOpenGLNativeWidget(ui->widget);
+    //setCentralWidget(ui->widget);
+
+    auto renderWindow = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    vtkWidget->setRenderWindow(renderWindow);
+
+    auto renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderWindow->AddRenderer(renderer);
+
+    auto sphereSource = vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetRadius(0.5);
+    sphereSource->Update();
+
+    auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(sphereSource->GetOutputPort());
+
+    auto actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+
+    renderer->AddActor(actor);
+    renderer->ResetCamera();
 }
 
 MainWindow::~MainWindow()
