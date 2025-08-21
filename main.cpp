9b7c6a9b6adb9c11a32c2195e654c396c3426012
ggyy <alqaz@outlook.com>
2025-08-21 21:32:47 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,14 +2,26 @@
 
 #include <QApplication>
 #include <qquickwindow.h>
+#include <QStyleFactory>
+#include <ktexteditor/Document>
+#include <ktexteditor/Editor>
+
+void fakecall()
+{
+	auto instance = KTextEditor::Editor::instance();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QQuickWindow::setSceneGraphBackend("opengl");
+    fakecall();
+	QQuickWindow::setSceneGraphBackend("opengl");
     qputenv("QSG_RHI_BACKEND", "opengl");
     /*QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);*/
+    auto styles = QStyleFactory::keys();
+	auto style = QStyleFactory::create("Breeze");
+    qApp->setStyle(style);
     MainWindow w;
     
     w.show();
